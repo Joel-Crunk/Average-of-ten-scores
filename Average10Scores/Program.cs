bb9321981ac7d29c8554699bf8d20429154b9103
Joel-Crunk <joel.crunk@outlook.com>
2019-02-28 15:58:36 -0500

    This commit will take 10 scores and average them andgive the grade

--- Average10Scores/Program.cs
+++ Average10Scores/Program.cs
@@ -4,12 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Average10Scores
+namespace AverageScore
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int numberint = 0;
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+
+
+
+                Console.Write($"\n Enter score number {i + 1}\n If no more scores enter 0: ");
+                int score = int.Parse(Console.ReadLine());
+                //string exit = Console.ReadLine();
+                if (score == 0)
+                {
+
+                    i = i + 10;
+                    numberint = numberint - 1;
+                }
+
+                numberint++;
+
+
+                sum = sum + score;
+
+
+            }
+
+            double ave = sum / numberint;
+            if (ave > 89)
+            {
+                Console.WriteLine($"\n\n{ave} is an \"A\" grade");
+            }
+            else if (ave > 79 && ave < 90)
+            {
+                Console.WriteLine($"\n\n{ave} is a \"B\" grade");
+            }
+            else if (ave > 69 && ave < 80)
+            {
+                Console.WriteLine($"\n\n{ave} is a \"C\" grade");
+            }
+            else if (ave > 59 && ave < 70)
+            {
+                Console.WriteLine($"\n\n{ave} is a \"D\" grade");
+            }
+            else
+            {
+                Console.WriteLine($"\n\n{ave} is a \"F\" grade");
+            }
+
+            Console.ReadLine();
+            //Console.WriteLine(ave);
+
         }
     }
 }
